d270f263dfe1dafa0647f96344e5a74b5d1cc7f3
winddyx <59448294+winddyx@users.noreply.github.com>
2024-07-25 05:24:43 +0000

    23054d2

--- setup_env.sh
+++ setup_env.sh
@@ -1,5 +1,4 @@
 sudo -E apt-get update
-
 sudo -E apt-get install -y ack antlr3 asciidoc autoconf automake autopoint binutils bison build-essential \
 bzip2 ccache cmake cpio curl device-tree-compiler fastjar flex gawk gettext gcc-multilib g++-multilib \
 git gperf haveged help2man intltool libc6-dev-i386 libelf-dev libfuse-dev libglib2.0-dev libgmp3-dev \
@@ -7,11 +6,7 @@ libltdl-dev libmpc-dev libmpfr-dev libncurses5-dev libncursesw5-dev libpython3-d
 libssl-dev libtool lrzsz mkisofs msmtp ninja-build p7zip p7zip-full patch pkgconf python2.7 python3 \
 python3-pyelftools python3-setuptools qemu-utils rsync scons squashfs-tools subversion swig texinfo \
 uglifyjs upx-ucl unzip vim wget xmlto xxd zlib1g-dev
-
 rm -rf /tmp/openwrt
-
-cd /tmp && git clone https://github.com/openwrt/openwrt.git -b v17.01.7
-
+cd /tmp && git clone https://github.com/openwrt/openwrt.git -b v23.05.4
 cd /tmp/openwrt && ./scripts/feeds update -a && ./scripts/feeds install -a
-
 cd /tmp/openwrt && make menuconfig
